a0cb8f73e7e5fadb92a131fe37dc458c72f67154
Yavuz Ozdemir <120899712+islam415@users.noreply.github.com>
2023-12-06 22:51:48 +0300

    .

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class CharacterController : MonoBehaviour
     {
         [SerializeField] private float _speed = 2f;
+
         private Animator _playerAnim;
+        private Rigidbody _rb;
 
         private void Awake()
         {
             _playerAnim = GetComponent<Animator>();
+            _rb = GetComponent<Rigidbody>();
         }
 
         private void Update()
@@ -23,12 +25,16 @@ namespace DefaultNamespace
 
             else if (touch.phase == TouchPhase.Stationary)
             {
-                Vector3 movement = Vector3.forward * (_speed * Time.deltaTime);
-                transform.Translate(movement);
+                Vector3 movement = _rb.velocity;
+                movement.z = _speed;
+                _rb.velocity = movement;
             }
 
             else if (touch.phase == TouchPhase.Ended)
+            {
                 _playerAnim.SetBool("isWalking", false);
+                _rb.velocity = Vector3.zero;
+            }
         }
     }
 }
