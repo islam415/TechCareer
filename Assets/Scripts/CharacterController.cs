7148227df626a27290138326faceef985078fa87
Yavuz Ozdemir <120899712+islam415@users.noreply.github.com>
2023-12-06 22:21:43 +0300

    .

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CharacterController : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 2f;
+        private Animator _playerAnim;
+
+        private void Awake()
+        {
+            _playerAnim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (Input.touchCount < 1) return;
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+                _playerAnim.SetBool("isWalking", true);
+
+            else if (touch.phase == TouchPhase.Stationary)
+            {
+                Vector3 movement = Vector3.forward * (_speed * Time.deltaTime);
+                transform.Translate(movement);
+            }
+
+            else if (touch.phase == TouchPhase.Ended)
+                _playerAnim.SetBool("isWalking", false);
+        }
+    }
+}
